891bf011b4f83fb36a58dc4373625bccdae459c4
PetrDockalik <petrdockalik@gmail.com>
2020-02-17 22:10:15 +0100

    [LAB] Adding 02-ise lab

---
+++ Labs/02-ise/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/xbolfm01/Digital-electronics-1/Labs/dnesni/top_TestBench1_isim_beh.exe" -prj "/home/lab661/Documents/xbolfm01/Digital-electronics-1/Labs/dnesni/top_TestBench1_beh.prj" "work.top_TestBench1" 
